0c3341752e8f270b650cce43cceb9c27d6a3e0c0
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-04 14:57:19 -0300

    Create media_final.js

---
+++ 04_08_2025 - Aula 1 - Javascript/Exercicios/media_final.js
@@ -0,0 +1,6 @@
+//crie um programa que solicite ao usuário as notas de três provas e exiba a média final
+let nota1 = parseFloat(prompt("Digite a primeira nota: "));
+let nota2 = parseFloat(prompt("Digite a segunda nota: "));
+let nota3 = parseFloat(prompt("Digite a terceira nota: "));
+let media = (nota1 + nota2 + nota3) / 3;
+alert("A média final é: " + media); 
