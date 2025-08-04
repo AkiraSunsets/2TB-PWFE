564f5294eb4af8bb970b305a836395ffefc57f2e
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-04 15:45:50 -0300

    Create Manipulacao_de_strings_e_arrays.js

---
+++ 04_08_2025 - Aula 1 - Javascript/Manipulacao_de_strings_e_arrays.js
@@ -0,0 +1,19 @@
+//Manipulação de strings e arrays
+
+let a = "a";
+let b = "b";
+
+let c = 25;
+
+console.log(a>b); //false
+console.log(a<b); //true
+
+console.log(typeof (a)); //tipos
+console.log(typeof (c.toString));
+console.log(arr.toString()); //converte para string
+console.log(arr); //converte para string
+console.log(arr.length); //tamanho do array
+console.log(arr.slice(1, 2)); //fatia o array
+console.log(nome.toUpperCase()); //converte para maiúsculas
+console.log(nome.toLowerCase()); //converte para minúsculas
+console.log(nome.replace("K", "G")); //substitui K por G
