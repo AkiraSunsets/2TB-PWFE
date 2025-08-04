2f6f5645cf44ef82c6abe186b21354fb2a0abdb3
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-04 14:16:15 -0300

    Create Exercicios.js

---
+++ 04_08_2025 - Aula 1 - Javascript/Exercicios.js
@@ -0,0 +1,18 @@
+//Exercicio 1 - Ex
+let nome = prompt("Digite seu nome:"); 
+console.log("Olá! " + nome);
+
+
+//Exercicio 2
+let materia = prompt("Qual é a sua matéria favorita?");
+console.log("A matéria {materia} realmente é ótima!");
+
+
+//Exercicio 3
+let data = prompt("Que dia é hoje?");
+console.log("Hoje é dia: " + data);
+
+
+//Exercicio 4
+let estado = prompt("Estás preparado para o segundo semestre de desenvolvimento de sistemas?");
+console.log("Você está preparado?" + estado);
