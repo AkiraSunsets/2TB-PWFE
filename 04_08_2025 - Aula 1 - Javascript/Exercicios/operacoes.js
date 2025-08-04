b3ff1b97c48b38c275f3ebc2a237c906ed482766
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-08-04 14:56:58 -0300

    Create operacoes.js

---
+++ 04_08_2025 - Aula 1 - Javascript/Exercicios/operacoes.js
@@ -0,0 +1,9 @@
+//peça ao usuário para inserir dois numeros e exiba o resultado das quatro operações matemáticas básicas (soma, subtração, multiplicação e divisão)
+
+let num1 = parseFloat(prompt("Digite o primeiro número: ")); //parsefloat converte a string para um número 
+let num2 = parseFloat(prompt("Digite o segundo número: ")); 
+
+alert("Soma: " + (num1 + num2));
+alert("Subtração: " + (num1 - num2));
+alert("Multiplicação: " + (num1 * num2));
+alert("Divisão: " + (num1/ num2));
